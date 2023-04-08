52c333237ce5c8fbd69166d04215077ad4529876
Yoga Yu <yoga.yu.169@gmail.com>
2023-04-08 20:53:01 +0800

    init workflow

---
+++ scripts/generate_docset.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set +e
+resp=$(curl -s https://api.github.com/repos/aws/aws-cdk/releases/latest)
+tag=$(echo $resp | jq -r '.tag_name' )
+wget "https://github.com/aws/aws-cdk/releases/download/${tag}/aws-cdk-python-docs-${tag:1}.zip" -O ../doc.zip
+wget "https://github.com/aws/aws-cdk/releases/download/${tag}/aws-cdk-python-docs-${tag:1}.zip.sig" -O ../doc.zip.sig
+gpg --verify ../doc.zip.sig ../doc.zip
+mkdir ../export
+unzip ../doc.zip -d ../export
+echo ./static/specific.css >> ../export/_static/css/custom.css
+
+pip3 install -r requirements.txt
+doc2dash -n "AWS_CDK_Python" -I index.html -j -u "https://docs.aws.amazon.com/cdk/api/v2/python/" ../export
+ls .
