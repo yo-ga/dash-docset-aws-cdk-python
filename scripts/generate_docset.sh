da10556c7aaa00cf3e07d59b6bd0be978a4dcb77
Yoga Yu <yoga.yu.169@gmail.com>
2023-04-09 14:44:41 +0800

    Init release (#1)
    * init workflow
    
    * feat: add icon
    
    * feat: auto release

---
+++ scripts/generate_docset.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+resp=$(curl -s https://api.github.com/repos/aws/aws-cdk/releases/latest)
+tag=$(echo $resp | jq -r '.tag_name' )
+wget "https://github.com/aws/aws-cdk/releases/download/${tag}/aws-cdk-python-docs-${tag:1}.zip" -O ../doc.zip
+mkdir ../export
+unzip ../doc.zip -d ../export
+echo ./static/specific.css >> ../export/_static/css/custom.css
+
+pip3 install -r requirements.txt
+doc2dash -n "AWS CDK Python" -I index.html -j -u "https://docs.aws.amazon.com/cdk/api/v2/python/" ../export
+mv ./AWS\ CDK\ Python.docset/ ./AWS_CDK_Python.docset/
+cp ./static/icon.png ./AWS_CDK_Python.docset/
+cp ./static/icon@2x.png ./AWS_CDK_Python.docset/
+ls .
+
+tar --exclude='.DS_Store' -cvzf AWS_CDK_Python.tgz AWS_CDK_Python.docset
