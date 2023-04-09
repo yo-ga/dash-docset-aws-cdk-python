054c49d36a9ef592a4cbc428f549ccf85fd220bc
Yoga Yu <yoga.yu.169@gmail.com>
2023-04-09 12:06:08 +0800

    chore: without verify

--- scripts/generate_docset.sh
+++ scripts/generate_docset.sh
@@ -4,8 +4,6 @@ set -e
 resp=$(curl -s https://api.github.com/repos/aws/aws-cdk/releases/latest)
 tag=$(echo $resp | jq -r '.tag_name' )
 wget "https://github.com/aws/aws-cdk/releases/download/${tag}/aws-cdk-python-docs-${tag:1}.zip" -O ../doc.zip
-wget "https://github.com/aws/aws-cdk/releases/download/${tag}/aws-cdk-python-docs-${tag:1}.zip.sig" -O ../doc.zip.sig
-gpg --verify ../doc.zip.sig ../doc.zip
 mkdir ../export
 unzip ../doc.zip -d ../export
 echo ./static/specific.css >> ../export/_static/css/custom.css
